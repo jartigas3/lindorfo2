19905e6d155f158719594e602ca5924130cd4910
jartigas3 <jpartigas@miuandes.cl>
2025-01-14 17:59:59 -0300

    terminada alpha avatar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,23 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textContainer = document.getElementById("dynamic-text");
+    if (textContainer) {
+      const text = "Hola, soy Sebastián Jiménez, ¿cómo te puedo ayudar?"; // Texto que se escribirá
+      let index = 0;
+  
+      function typeEffect() {
+        if (index < text.length) {
+          textContainer.innerHTML += text.charAt(index); // Añade un carácter al texto
+          index++;
+          setTimeout(typeEffect, 30); // Ajusta la velocidad de escritura (en milisegundos)
+        }
+      }
+  
+      typeEffect(); // Inicia el efecto de escritura
+    }
+  });
+  
