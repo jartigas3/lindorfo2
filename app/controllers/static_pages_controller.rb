d15c785031ddf828ad9659ea7c9c28773714e199
jartigas3 <jpartigas@miuandes.cl>
2025-01-14 17:24:42 -0300

    alpha lindorfo2

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,34 @@
+class StaticPagesController < ApplicationController
+    def home
+     
+    end
+  
+    def quienes_somos
+     
+    end
+
+    def servicios
+     
+    end
+  
+    def clientes
+     
+    end
+
+    def blog
+     
+    end
+  
+    def movistarvet
+     
+    end
+
+    def mas
+     
+    end
+  
+    def atencion_virtual
+
+    end
+  end
+  
